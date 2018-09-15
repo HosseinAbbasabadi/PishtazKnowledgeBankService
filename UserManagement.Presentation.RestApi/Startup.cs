d7bc9fbbb36b45c17b002e6d713fc44bf98b68c4
PezhvakPC <Hossein@DESKTOP-8LS1VEJ>
2018-09-14 21:31:58 -0700

    Init Development Branch;

--- UserManagement.Presentation.RestApi/Startup.cs
+++ UserManagement.Presentation.RestApi/Startup.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Castle.Windsor;
 using Framework.Castle;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
