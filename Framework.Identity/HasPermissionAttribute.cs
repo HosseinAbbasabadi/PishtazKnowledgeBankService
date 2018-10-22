87a7f2b7780d43a99ccb46cfe241a8b3a8ffb6ab
Hossein Pezhvak Pc <hossein.abbasabadi@outlook.com>
2018-10-22 07:11:41 +0330

    HasPermissionAttrubute Added;

---
+++ Framework.Identity/HasPermissionAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.Identity
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class HasPermissionAttribute : Attribute
+    {
+        public Enum Permission { get; private set; }
+
+        public HasPermissionAttribute(object value)
+        {
+            if (value.GetType().IsEnum)
+                Permission = (Enum) value;
+            else
+                throw new Exception();
+        }
+    }
+}
